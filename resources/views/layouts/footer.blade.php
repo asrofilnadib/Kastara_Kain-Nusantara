0026af211fdeadf9732037e00c87345723722b02
Aqil Rahmatullah <aqilr54@gmail.com>
2023-12-06 01:52:18 +0800

    Making Content in About Page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,9 +3,9 @@
   <div class="container">
     <div class="p-t-40">
       <p class="stext-107 cl6 txt-center">
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made by Kastara Team
+
 
       </p>
     </div>
