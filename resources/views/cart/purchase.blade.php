87ffccd0d0446048710c4dbf04c88c7af9839f4e
Aqil Rahmatullah <aqilr54@gmail.com>
2023-12-08 15:08:20 +0800

    update register page, purchase page, navbars, order page, product page.

--- resources/views/cart/purchase.blade.php
+++ resources/views/cart/purchase.blade.php
@@ -2,6 +2,11 @@
 @section('title', $viewData["title"])
 @section('subtitle', $viewData["subtitle"])
 @section('content')
+<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('/../images/bg-04.jpg');">
+    <h2 class="ltext-105 cl0 txt-center">
+      Cart
+    </h2>
+  </section>
 <div class="card">
   <div class="card-header">
     Purchase Completed
