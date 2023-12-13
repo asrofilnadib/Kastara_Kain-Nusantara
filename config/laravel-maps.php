d2feff01a6fe0108eb2099341b443aef198063a1
asrofilnadib <60963555+asrofilnadib@users.noreply.github.com>
2023-12-13 20:08:22 +0700

    Update laravel-maps.php

--- config/laravel-maps.php
+++ config/laravel-maps.php
@@ -9,6 +9,6 @@ return [
         'access_token' => env('MAPS_MAPBOX_ACCESS_TOKEN', null),
     ],
     'google_maps' => [
-        'access_token' => env('MAPS_GOOGLE_MAPS_ACCESS_TOKEN', 'AIzaSyB5ygQJvtQK8L9VPk5Xhhw_vk-4EsnX57A')
+        'access_token' => env('MAPS_GOOGLE_MAPS_ACCESS_TOKEN', '')
     ]
 ];
